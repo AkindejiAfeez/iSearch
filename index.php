baf11d3d75aad54010e6b1b442813397076cc8cf
AkindejiAfeez <harpheez28@gmail.com>
2024-10-16 17:35:02 +0300

    Search Engine

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Spider Search</title>
+
+	<meta name="description" content="Search the web for sites and images.">
+	<meta name="keywords" content="Search engine, Spider Crawl, websites">
+	<meta name="author" content="Akindeji Afeez">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+<body>
+
+	<div class="wrapper indexPage">
+	
+
+		<div class="mainSection">
+
+			<div class="logoContainer">
+				<img src="assets\images\logo.png">
+			</div>
+
+
+			<div class="searchContainer">
+
+				<form action="search.php" method="GET">
+
+					<input class="searchBox" type="text" name="term" placeholder="Search the web....">
+					<input class="searchButton" type="submit" value="Search">
+
+
+				</form>
+
+			</div>
+
+
+		</div>
+
+
+	</div>
+
+</body>
+</html>
